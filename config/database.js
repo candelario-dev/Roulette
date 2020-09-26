0c870d020ec05da1785d8ee041c7ff53d837db1b
Rebecca Candelario <69063941+candelario-dev@users.noreply.github.com>
2020-09-25 20:36:06 -0400

    First commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://dbroulette:roulette@cluster0.aoz4o.mongodb.net/roulette?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'roulette'
+};
